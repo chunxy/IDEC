f8fd8ba05ae21b58e0849c95481eeccf86f6ce40
ChunXiao Ye <chunxy@ssd32.dbg.private>
2025-05-21 14:34:58 +0800

    add scripts

---
+++ script-crawl.sh
@@ -0,0 +1,12 @@
+source $MYDISK/miniconda3/etc/profile.d/conda.sh
+conda activate system
+
+python idec.py \
+--dataset crawl \
+--n_clusters 2000 \
+--batch_size 256 \
+--n_z 10 \
+--gamma 0.1 \
+--update_interval 1 \
+--tol 0.001 \
+--lr 0.001 \
